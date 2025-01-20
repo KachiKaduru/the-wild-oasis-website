1110b6ebcd4bf53e6c3e35a172e0c3671e609500
Kachii <kachikaduru@gmail.com>
2025-01-20 10:42:51 +0100

    Implemented the dynamic segment for each cabin.

--- app/cabins/[cabinId]/page.jsx
+++ app/cabins/[cabinId]/page.jsx
@@ -1,4 +1,6 @@
+import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 // PLACEHOLDER DATA
 const cabin = {
@@ -13,15 +15,15 @@ const cabin = {
     "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
 };
 
-export default function Page() {
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
+export default async function Page({ params }) {
+  const cabin = await getCabin(params.cabinId);
+  const { id, name, maxCapacity, regularPrice, discount, image, description } = cabin;
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <img src={image} alt={`Cabin ${name}`} />
+          <Image src={image} fill className="object-cover" alt={`Cabin ${name}`} />
         </div>
 
         <div>
@@ -35,15 +37,13 @@ export default function Page() {
             <li className="flex gap-3 items-center">
               <UsersIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
-                guests
+                For up to <span className="font-bold">{maxCapacity}</span> guests
               </span>
             </li>
             <li className="flex gap-3 items-center">
               <MapPinIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                Located in the heart of the{" "}
-                <span className="font-bold">Dolomites</span> (Italy)
+                Located in the heart of the <span className="font-bold">Dolomites</span> (Italy)
               </span>
             </li>
             <li className="flex gap-3 items-center">
@@ -57,9 +57,7 @@ export default function Page() {
       </div>
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
-        </h2>
+        <h2 className="text-5xl font-semibold text-center">Reserve today. Pay on arrival.</h2>
       </div>
     </div>
   );
