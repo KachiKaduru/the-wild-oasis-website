9828d204f5f7a49ae011b348239fee999a9348ea
Kachii <kachikaduru@gmail.com>
2025-01-25 23:45:13 +0100

    Using the Context API for State Management

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import Header from "./_components/Header";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import { ReservationProvider } from "./_contexts/ReservationContext";
+
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
@@ -25,7 +27,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
